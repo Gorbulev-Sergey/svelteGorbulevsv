9d1a35f1cc1e4c2d3b79ad3cc8ba36e54dba52d7
Горбулёв Сергей <gorbulevsv@yandex.ru>
2023-06-24 12:37:13 +0300

    1

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,40 @@
+<script>
+	import After from '$lib/components/After.svelte';
+	import Before from './Before.svelte';
+
+	/**
+	 * @type {any[]}
+	 */
+	export let routes = [];
+	let colorBrand = 'rgba(255,46,46,.4)';
+</script>
+
+<Before>
+	<nav
+		class="position-fixed top-0 w-100 d-flex flex-column align-items-center justify-content-between bg-light bg-opacity-10 gap-1 px-3 py-2">
+		<a href="/" class="btn btn-light text-dark border-0 text-uppercase" style="background:{colorBrand}">
+			<b>Всем привет</b>
+		</a>
+	</nav>
+	<nav
+		class="position-fixed bottom-0 w-100 d-flex flex-column align-items-center justify-content-between bg-light bg-opacity-10 gap-1 px-3 py-2">
+		<div class="d-flex align-items-center gap-2">
+			{#each routes as item}
+				<a href="/{item.url}" class="btn btn-sm btn-light bg-light bg-opacity-50 border-0 text-dark">{item.text}</a>
+			{/each}
+		</div>
+	</nav>
+</Before>
+<After>
+	<nav
+		class="position-fixed top-0 w-100 d-flex align-items-center justify-content-between bg-light bg-opacity-10 px-3 py-2">
+		<a href="/" class="btn btn-light text-dark border-0 text-uppercase" style="background:{colorBrand}">
+			<b>Всем привет</b>
+		</a>
+		<div class="d-flex align-items-center gap-2">
+			{#each routes as item}
+				<a href="/{item.url}" class="btn btn-light bg-light bg-opacity-50 border-0 text-dark">{item.text}</a>
+			{/each}
+		</div>
+	</nav>
+</After>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,12 @@
-<div class="container my-3">
+<script>
+	import Nav from '$lib/components/Nav.svelte';
+	let routes = [
+		{ text: 'Me', url: 'gorbulevsv.ru' },
+		{ text: 'LandingPage', url: 'landingPage1.gorbulevsv.ru' }
+	];
+</script>
+
+<Nav {routes} />
+<div class="container" style="margin-top:4em; margin-bottom:4em;">
 	<slot />
 </div>
